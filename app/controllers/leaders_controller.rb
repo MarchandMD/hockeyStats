bcb40fd553d0409bdf73d130505091bb64a4dc98
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-02-02 08:37:11 -0700

    add some padding to welcome#index view

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -2,6 +2,6 @@ class LeadersController < ApplicationController
   def index
     @leaders = Celly::Leaders.new
 
-    @current_goals_leaders = @leaders.goals(20192018)[:data]
+    @current_goals_leaders = @leaders.goals(20212022)[:data]
   end
 end
