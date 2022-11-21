4ec4005b430d654c9786d1a0e3c9a9803de023a1
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-20 18:17:27 -0700

    Streamlines TeamsController to use SRP

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,20 +1,10 @@
 class TeamsController < ApplicationController
-  require 'rest-client'
-  require 'json'
-  require 'dotenv/load'
 
   def index
-    @teams = RestClient.get("#{ENV['BASE']}/teams")
-    # render :json => JSON.pretty_generate(JSON.parse(@teams_direct))
-    @teams = JSON.parse(@teams)
-    @teams = @teams['teams']
+    @teams = TeamSearch.new.teams_info
   end
 
   def show
-    roster_modifier = '?expand=team.roster'
-    @team = RestClient.get("#{ENV['BASE']}/teams/#{params[:id]}#{roster_modifier}")
-    # render json: JSON.pretty_generate(JSON.parse(@team))
-    @team= JSON.parse(@team)
-    @roster = @team['teams'][0]['roster']['roster']
+    @team = TeamSearch.new.team_info(params[:id])
   end
 end
