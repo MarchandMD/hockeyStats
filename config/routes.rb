85ad447aacb0c377d71d93cf0b2b1567b412334e
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-20 18:15:54 -0700

    Nests players resource in teams resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'links/index'
   get 'about/index'
   root to: 'welcome#index'
-  resources :teams, only: %i[index show]
-  resources :players, only: %i[index show]
+  resources :teams, only: %i[index show] do
+    resources :players, only: %i[index show], controller: 'rosters'
+  end
   resources :standings, only: %i[index show]
   resources :leaders, only: [:index]
   resources :schedule, only: [:index]
