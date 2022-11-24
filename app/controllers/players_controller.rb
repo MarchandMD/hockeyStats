825b70b99de7fb404a653339fb49d583b07c9ac7
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-24 10:30:23 -0700

    Updates players controller and improves some views

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,25 +1,5 @@
 class PlayersController < ApplicationController
-  require 'net/https'
-  require 'json'
-
-  def index
-    base = "https://statsapi.web.nhl.com/api/v1"
-    endpoint = "/people/8477472"
-    uri = URI("#{base}/#{endpoint}")
-    @data = Net::HTTP.get(uri)
-    @data = JSON.parse(@data)
-  end
-
   def show
-    base = "https://statsapi.web.nhl.com/api/v1"
-    endpoint = "/people/#{params[:id]}"
-    uri = URI("#{base}/#{endpoint}")
-    @data = Net::HTTP.get(uri)
-    @data = JSON.parse(@data)
-    stats_endpoint = "#{endpoint}/stats"
-    stats_uri = URI("#{base}/#{stats_endpoint}?stats=yearByYear")
-    @stats = Net::HTTP.get(stats_uri)
-    @stats = JSON.parse(@stats)
+    @player_stats = PlayerSearch.new.player_stats(params[:id])
   end
-
 end
