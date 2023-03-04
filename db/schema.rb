c507e8670a9b9553406015bebfb797a47f187c52
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-03-04 06:01:03 -0700

    Updates readme, adds Game model, with unit test
    Why is this PR being made?
    wip - moving from unit test to integration testing
    
    How is this accomplished?
    unit testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_144311) do
+ActiveRecord::Schema.define(version: 2023_03_04_123119) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "home"
+    t.string "away"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "predictions", force: :cascade do |t|
     t.datetime "created_at", null: false
