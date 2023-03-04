5f1576bdc069725f01f3f070498ac091ce5197b0
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-03-04 14:50:06 -0700

    Updates facade and game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,14 @@
 class Game < ApplicationRecord
   enum status: {
     scheduled: 1,
-    in_progress: 4,
-    finished: 7
+    status_two: 2,
+    status_three: 3,
+    status_four: 4,
+    status_five: 5,
+    final: 6,
+    finished: 7,
+    status_eight: 8,
+    status_nine: 9,
+    status_zero: 0
   }
 end
