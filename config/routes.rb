badd9e4dbacdd222f62561cfb170857469c34329
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-03-06 11:20:16 -0700

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   get 'links/index'
   get 'about/index'
   root to: 'welcome#index'
-  resources :teams, only: %i[index show] do
-    resources :players, only: %i[index], controller: 'rosters'
-    resources :players, only: %i[show]
-  end
-  resources :standings, only: %i[index show]
-  resources :leaders, only: [:index]
-  resources :schedule, only: [:index]
+
   resources :scores, only: %i[index show post]
-  resources :configurations, only: [:index]
-  resources :predictions
+
+  resources :users, only: %i[index show create] do
+    resources :predictions, only: %i[index new create]
+  end
 end
